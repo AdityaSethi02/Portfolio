ec87cfef34d4f742275b4e6ce19823e0ffe9f144
Aditya Sethi <adityasethi2004@gmail.com>
2024-11-08 19:49:02 +0530

    init

---
+++ components/Resume.tsx
@@ -0,0 +1,14 @@
+
+export const Resume = () => {
+    const handleClick = () => {
+        window.open("https://docs.google.com/document/d/1Mn0t_oQDgAwXjfIqHsyQ5xoh1m48luTHD12fht91m0A/edit?tab=t.0", "_blank")
+    }
+
+    return (
+      <a href="/Aditya Sethi_Resume.pdf" download>
+        <button onClick={handleClick} className="bg-blue-500 text-white text-3xl px-6 py-3 rounded-2xl shadow-lg hover:bg-blue-600 ">
+            Resume
+        </button>
+      </a>
+    );
+  };
